8d0c23d363d7e090404cf0f069a7fb5f430ae22d
bhokaresneha <bhokaresneha20@gmail.com>
2022-05-31 23:58:23 +0530

    Added :CRUD Operation:Retriveing All data from table

--- src/main/java/com/bridgelabz/EmployeePayrollDBServices.java
+++ src/main/java/com/bridgelabz/EmployeePayrollDBServices.java
@@ -1,8 +1,11 @@
 package com.bridgelabz;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeePayrollDBServices implements EmployeePayrollServiceInterface{
 
@@ -29,7 +32,23 @@ public class EmployeePayrollDBServices implements EmployeePayrollServiceInterfac
             throw new RuntimeException(e);
         }
     }
-
+    public static List<EmployeePayroll> retriveData(String sql) {
+        List<EmployeePayroll> employeeDataList=new ArrayList<>();
+        try{
+            Connection connection=JDBCConnection.getConnection();
+            Statement statement=connection.createStatement();
+            ResultSet resultSet=statement.executeQuery(sql);
+            while (resultSet.next()){
+                int id=resultSet.getInt("id");
+                String name=resultSet.getString("name");
+                double salary=resultSet.getDouble("salary");
+                employeeDataList.add(new EmployeePayroll(id,name,salary));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return employeeDataList;
+    }
 
 }
 
--- src/test/java/EmployeePayrollServiceTest.java
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -1,13 +1,17 @@
+import com.bridgelabz.EmployeePayroll;
 import com.bridgelabz.EmployeePayrollDBServices;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class EmployeePayrollServiceTest {
+import java.util.List;
 
+public class EmployeePayrollServiceTest
+{
     EmployeePayrollDBServices employeePayrollDBServices= new EmployeePayrollDBServices();
 
     @Test
-    public void createTableInDB(){
+    public void createTableInDB()
+    {
         String sql="create table employee_payroll(id INT unsigned NOT NULL AUTO_INCREMENT,name VARCHAR(150) NOT NULL," +
                      "salary Double NOT NULL,start DATE NOT NULL, PRIMARY KEY (id))";
         Assertions.assertTrue(employeePayrollDBServices.createTable(sql));
@@ -19,6 +23,18 @@ public class EmployeePayrollServiceTest {
         String sql="INSERT INTO employee_payroll ( name, salary, start ) VALUE( 'Bill', 1000000.00, '2010-10-30' )," +
                 "( 'Tersia', 2000000.00, '2019-11-13' ),( 'charlie', 3000000.00, '2020-05-21' )";
         Assertions.assertTrue(employeePayrollDBServices.insertRecord(sql));
-
+    }
+    @Test
+    public void retriveDataFromTable()
+    {
+       String sql="SELECT * FROM employee_payroll";
+        List<EmployeePayroll> employeeData=employeePayrollDBServices.retriveData(sql);
+            for(EmployeePayroll data:employeeData)
+            {
+                System.out.println(data);
+            }
+            Assertions.assertEquals(3,employeeData.size());
     }
 }
+
+
